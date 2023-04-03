65cce3d49e409b2a2dd8d7204f0079164a9db818
Umzzang <newjeff1993@gmail.com>
2023-04-03 22:16:10 +0900

    스택_후위표기식

--- 백준/class4/자료구조/1918_후위표기식.py
+++ 백준/class4/자료구조/1918_후위표기식.py
@@ -1,39 +1,32 @@
-from collections import deque
+# 왜케 어렵지;
 
 cal = list(input())
+stack = []
+ans = ''
+for s in cal:
+    if s.isalpha():
+        ans += s
+    elif s == '(':
+        stack.append(s)
+    elif s == ')':
+        while stack and stack[-1] != '(':
+            ans += stack.pop()
+        stack.pop()
+    elif s == '*' or s == '/':
+        while stack and (stack[-1] == '*' or stack[-1] == '/'):
+            ans += stack.pop()
+        stack.append(s)
+    elif s == '+' or s == '-':
+        while stack and stack[-1] != '(':
+            ans += stack.pop()
+        stack.append(s)
 
-c = ['+', '-', '*', '/']
-
-i = 0
-while i < len(cal):
-    if cal[i] == '*' or cal[i] == '/':
-        cal.insert(i+2, ')')
-        cal.insert(i-1, '(')
-        i += 1
-    i += 1
-answer = ''
-
-
-
-
-# def make(lst):
-#     print(lst)
-#     if lst[0] == '(' or lst[0] == ')':
-#         return make(lst[1:])
-#     if len(lst) == 0:
-#         return
-#     if len(lst) == 3:
-#         ans = str(lst[0]) + str(lst[1] + str(lst[2]))
-#         return ans
-
-#     if lst[1] in c:
-#         return str(lst[0])  + make(lst[2:]) + str(lst[1])
-    
-   
-    
-# ans = make(cal)
+while stack:
+    ans += stack.pop()
 
 print(ans)
+
+
 
 
 
