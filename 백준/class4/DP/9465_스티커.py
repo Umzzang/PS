4cf588e949f033bdc502bede2d5167b619cddb8e
Umzzang <newjeff1993@gmail.com>
2023-04-07 17:30:47 +0900

    dp_스티커

---
+++ 백준/class4/DP/9465_스티커.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    dp = []
+    for _ in range(2):
+        dp.append(list(map(int, input().split())))
+    
+    
+
+
+    if n > 1:
+        dp[0][1] = dp[1][0] + dp[0][1]
+        dp[1][1] = dp[1][1] + dp[0][0]
+    if n > 2:
+        for i in range(2, n):
+            dp[0][i] += max(dp[1][i-1], dp[1][i-2])
+            dp[1][i] += max(dp[0][i-1], dp[0][i-2])
+        # print(dp)
+    print(max(dp[0][n-1], dp[1][n-1]))
